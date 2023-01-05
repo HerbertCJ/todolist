2a361934c4c76e12460db7595fb1d5cec4724547
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-05 11:50:22 -0300

    updated form function

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,21 @@
-export default function Form() {
+import PropTypes from 'prop-types'
+import { FaPlus } from 'react-icons/fa'
+
+import './Form.css'
+
+export default function Form({ handleSubmit, handleChange, text }) {
     return (
         <div>
-            <form action="#" className="form">
-                <input type="text" />
-                <button type="submit">ADD</button>
+            <form action="#" onSubmit={handleSubmit} className="form">
+                <input type="text" onChange={handleChange} value={text} />
+                <button type="submit"><FaPlus /></button>
             </form>
         </div>
     )
+}
+
+Form.propTypes ={
+    handleSubmit: PropTypes.func.isRequired,
+    handleChange: PropTypes.func.isRequired,
+    text: PropTypes.string.isRequired,
 }
