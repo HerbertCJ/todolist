fe78a8c0b63105fc9d8b65d1b57cadbd7908bb76
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-05 11:49:54 -0300

    updated item function

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,28 @@
-export default function Item() {
+import React from 'react';
+import { FaEdit, FaWindowClose } from 'react-icons/fa' 
+import PropTypes from 'prop-types'
+
+import './Item.css'
+
+export default function Item({list, handleEdit, handleDelete}) {
     return (
         <ul className="list">
-            <li>Test</li>
+            {list.map((item, index) => (
+                <li key={item}>
+                    {item}
+                    <span>
+                        <FaEdit onClick={(e) => handleEdit(e, index)} className="edit" />
+                        <FaWindowClose onClick={(e) => handleDelete(e, index)} className="delete" />
+                    </span>
+                </li>
+            ))}
         </ul>
     )
+}
+
+Item.propTypes = {
+    list: PropTypes.array,
+    handleEdit: PropTypes.func.isRequired,
+    handleDelete: PropTypes.func.isRequired,
+    
 }
