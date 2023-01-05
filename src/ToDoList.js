a5b06e19b5d88af4c9902b168bef083a2ddd574d
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-05 11:47:44 -0300

    created add, remove function

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -11,13 +11,51 @@ export default class ToDoList extends Component {
         index: -1,
     }
 
+    handleDelete = (e, index) => {
+        let { list } = this.state;
+        let newList = [...list]
+        newList.splice(index, 1)
 
-    render() {        
-        return (
+        this.setState({
+            list: [...newList],
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const { list, index } = this.state;
+        let { text } = this.state;
+
+        text = text.trim();
+        if (text === '') return;
+        if (list.indexOf(text) !== -1) return;
+
+        const newList = [...list]        
+        if (index === -1) {
+            this.setState({
+                list: [...newList, text],
+                text: ''                
+            })            
+        } else {
+            newList[index] = text;
+            this.setState({
+                list: [...newList],
+                index: -1,
+                text: '',
+            })
+        }
+    }
+    
+
+
+    render() {
+        const { text, list } = this.state;
+        return (            
             <div className="container">
                 <h1>To Do List</h1>
-                <Form />
-                <Item />
+                <Form handleSubmit={this.handleSubmit} handleChange={this.handleChange} text={text} />
+                <Item list={list} handleEdit={this.handleEdit} handleDelete={this.handleDelete} />
             </div>
         )
     }
